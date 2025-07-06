7661bbad5b65ff20050c160ab4d379bd12c7fa88
Bharath Thatha <149349975+Bharath-Thatha@users.noreply.github.com>
2025-07-06 14:48:40 +0530

    Add files via upload
    Week 3 Hands on uploaded

---
+++ DeepSkilling Hands-on/Week 3/spring-data-jpa-handson/Difference between JPA, Hibernate and Spring Data JPA/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<String> addEmployee(@RequestBody Employee employee) {
+        employeeService.addEmployee(employee);
+        return ResponseEntity.ok("Employee added successfully.");
+    }
+}
